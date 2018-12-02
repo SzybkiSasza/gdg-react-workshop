62650e2d1aa86f45089f4fc9dfaa8e81b0604b51
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2018-12-02 11:29:05 +0100

    Added all the stylesheets

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ import logo from './logo.svg';
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.tsx</code>
         </p>
       </div>
