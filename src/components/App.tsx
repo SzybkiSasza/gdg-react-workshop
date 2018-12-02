218394ce8a661efd515c021eb43635da35a942e5
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2018-12-02 12:19:48 +0100

    Moved app to components

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <header className='App'>
+        <h1>Hello World!</h1>
+      </header>
+    );
+  }
+}
+
+export default App;
