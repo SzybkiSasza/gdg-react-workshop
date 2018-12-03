2e2593968e0d5ba9881eba24da49cb3ab494ef28
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2018-12-03 21:06:47 +0100

    Added CSS Baseline.

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,15 +1,18 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
 import * as React from 'react';
-import './App.css';
 
 import Header from '../components/Header/Header';
+
+import './App.css';
 
 class App extends React.Component {
   public render() {
     return (
-      <div>
-        <Header/>
-        <section>{this.props.children}</section>
-      </div>
+      <React.Fragment>
+          <CssBaseline />
+          <Header/>
+          <section>{this.props.children}</section>
+      </React.Fragment>
     );
   }
 }
