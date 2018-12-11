75640bcf5500c9c5c99f435381b75a80a1c97eeb
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2018-12-11 21:39:21 +0100

    Header adjusted - fixed Material UI errors about the value.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,29 @@
 import * as React from 'react';
+import { ComponentProps } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 
 import { AppBar, Tabs, Toolbar, Typography } from '@material-ui/core';
 import { Home, PhotoLibrary } from '@material-ui/icons';
 
 import LinkTab from '../LinkTab/LinkTab';
-
-import { ComponentProps } from 'react';
 import './Header.css';
 
-interface HeaderProps extends RouteComponentProps, ComponentProps<any> {}
+interface HeaderProps extends RouteComponentProps, ComponentProps<any> {
+}
+
+const tabLinks = ['/', '/browser'];
 
 class Header extends React.Component<HeaderProps> {
+  get linkValue() {
+    const pathName = this.props.history.location.pathname;
+
+    if (tabLinks.indexOf(pathName) !== -1) {
+      return pathName;
+    }
+
+    return tabLinks[0];
+  }
+
   public render(): JSX.Element {
     return (
       <AppBar position='static'>
@@ -23,9 +35,9 @@ class Header extends React.Component<HeaderProps> {
           <nav className='Header__tabs'>
             <Tabs className='Header__tabs-wrapper'
                   fullWidth={true}
-                  value={this.props.history.location.pathname}>
-              <LinkTab icon={<Home/>} value='/'/>
-              <LinkTab icon={<PhotoLibrary/>} value='/browser'/>
+                  value={this.linkValue}>
+              <LinkTab icon={<Home/>} value={tabLinks[0]}/>
+              <LinkTab icon={<PhotoLibrary/>} value={tabLinks[1]}/>
             </Tabs>
           </nav>
         </Toolbar>
