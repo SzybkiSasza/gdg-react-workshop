9041fbd222aa52fa7913cd6fb7d986e51f14bc04
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2018-12-15 11:04:48 +0100

    Styled NotFound, added responsive behaviour.

--- src/layouts/NotFound/NotFound.tsx
+++ src/layouts/NotFound/NotFound.tsx
@@ -1,13 +1,25 @@
+import { Typography } from '@material-ui/core';
 import * as React from 'react';
 
 import droids from './droids.gif';
+import './NotFound.css';
 
 export default class NotFound extends React.Component {
   public render(): JSX.Element {
     return (
-        <div>
-            NOT FOUND
-          <img src={droids} alt='Owi Wan'/>
+        <div className='NotFound'>
+          <Typography className='NotFound__title' paragraph={true} variant='h1'>
+            404
+          </Typography>
+          <Typography className='NotFound__title' paragraph={true} variant='h2'>
+            These are not the droids
+          </Typography>
+          <div className='NotFound__image-wrapper'>
+            <img src={droids} alt='Obi Wan'/>
+          </div>
+          <Typography className='NotFound__title' paragraph={true} variant='h2'>
+            You're looking for
+          </Typography>
         </div>
     );
   }
