a225ba9ad1dc00a532d18a00af4c41d044037e00
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2019-10-15 04:23:47 +0200

    New component structure with proper handlers.

--- src/components/ImageUpload/ImageUpload.tsx
+++ src/components/ImageUpload/ImageUpload.tsx
@@ -1,27 +1,30 @@
 import { Dialog, DialogTitle, Fab } from '@material-ui/core';
 import { Add } from '@material-ui/icons';
-import { FunctionComponent, HTMLAttributes, useState } from 'react';
 import * as React from 'react';
+import { FunctionComponent, HTMLAttributes, useState } from 'react';
 
 import './ImageUpload.css';
 
 interface ImageUploadProps extends HTMLAttributes<HTMLDivElement> {
-    topPos?: number;
-    leftPos?: number;
+  topPos?: number;
+  leftPos?: number;
 }
 
 export const ImageUpload: FunctionComponent<ImageUploadProps> = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    return (
-        <>
-            <Fab className='ImageUpload' color='primary' onClick={ () => setOpen(true) }>
-                <Add/>
-            </Fab>
-            <Dialog open={ open }
-                onClose={ () => setOpen(false) }>
-                <DialogTitle>Choose image to upload</DialogTitle>
-            </Dialog>
-        </>
-    );
+  const handleClose = () => setOpen(false);
+  const handleOpen = () =>setOpen(true);
+
+  return (
+    <>
+      <Fab className='ImageUpload' color='primary' onClick={ handleOpen }>
+        <Add/>
+      </Fab>
+      <Dialog open={ open }
+              onClose={ handleClose }>
+        <DialogTitle>Choose image to upload</DialogTitle>
+      </Dialog>
+    </>
+  );
 };
