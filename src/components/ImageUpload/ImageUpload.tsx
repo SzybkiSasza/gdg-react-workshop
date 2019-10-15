9a3ec83582aaa52a751cf2c26e3d8c0debeb7f03
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2019-10-15 03:02:23 +0200

    Image Upload looks more Material-y now.

--- src/components/ImageUpload/ImageUpload.tsx
+++ src/components/ImageUpload/ImageUpload.tsx
@@ -1,48 +1,23 @@
-import { Button, Input } from '@material-ui/core';
-import classnames from 'classnames';
+import { Fab } from '@material-ui/core';
+import { Add } from '@material-ui/icons';
 import * as React from 'react';
+import { ClassAttributes } from 'react';
 
 import './ImageUpload.css';
 
-class ImageUpload extends React.Component<any> {
-  public render(): JSX.Element {
-    return (
-      <div className={classnames(this.props.className, { 'ImageUpload': true })}>
-        <Input
-          className='ImageUpload__path'
-          id='upload-path'
-          placeholder='PATH HERE'
-          readOnly={true}
-          value=''/>
+interface ImageUploadProps {
+    topPos: number;
+    leftPos: number;
+}
 
-        <div className='ImageUpload__buttons'>
-          <Button className='ImageUpload__button'
-                  color='secondary'
-                  onClick={this.selectFile}
-                  variant='contained'>
-            Browse
-          </Button>
-
-          <Button className='ImageUpload__button'
-                  color='secondary'
-                  onClick={this.uploadFile}
-                  variant='contained'>
-            Upload selected file
-          </Button>
-        </div>
-      </div>
-    );
-  }
-
-  private selectFile = (event: React.MouseEvent) => {
-    console.log('Selecting!');
-    console.log(event);
-  };
-
-  private uploadFile = (event: React.MouseEvent) => {
-    console.log('Uploading!');
-    console.log(event);
-  };
+class ImageUpload extends React.Component<ClassAttributes<ImageUploadProps>> {
+    public render(): JSX.Element {
+        return (
+            <Fab className='ImageUpload' color='primary'>
+                <Add/>
+            </Fab>
+        );
+    }
 }
 
 export default ImageUpload;
--- src/layouts/Browser/Browser.tsx
+++ src/layouts/Browser/Browser.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-import ImageBrowser from '../../components/ImageBrowser/ImageBrowser';
 import ImageUpload from '../../components/ImageUpload/ImageUpload';
 
 import './Browser.css';
@@ -9,7 +8,6 @@ export default class Browser extends React.Component {
   public render(): JSX.Element {
     return (
       <div className='Browser'>
-        <ImageBrowser className='Browser__images'/>
         <ImageUpload className='Browser__upload'/>
       </div>
     );
