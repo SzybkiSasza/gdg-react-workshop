949577c44aa1e54239484da1dbd1d2656579e59e
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2019-10-22 00:20:47 +0200

    Image Upload uses new service.

--- src/components/ImageUpload/ImageUpload.tsx
+++ src/components/ImageUpload/ImageUpload.tsx
@@ -1,19 +1,26 @@
 import { Fab } from '@material-ui/core';
 import { Add } from '@material-ui/icons';
 import * as React from 'react';
-import { FunctionComponent, HTMLAttributes } from 'react';
+import { ChangeEvent, FunctionComponent, HTMLAttributes } from 'react';
 
+import { StoredImage } from '../../models/image.model';
+import { ImagesService } from '../../services/images.service';
 import './ImageUpload.css';
 
 interface ImageUploadProps extends HTMLAttributes<HTMLDivElement> {
   topPos?: number;
   leftPos?: number;
+  onImagesSaved: (files: StoredImage[]) => void;
 }
 
-export const ImageUpload: FunctionComponent<ImageUploadProps> = () => {
+export const ImageUpload: FunctionComponent<ImageUploadProps> = ({ onImagesSaved }) => {
   let fileInput: HTMLInputElement | null;
 
-  const handleFiles = (files: FileList | null) => console.log(files);
+  const handleFiles = async (event: ChangeEvent<HTMLInputElement>) => {
+    const files = await ImagesService.instance.saveImages(event.target.files);
+    onImagesSaved(files);
+  };
+
   const handleOpen = () => fileInput && fileInput.click();
 
   return (
@@ -21,8 +28,13 @@ export const ImageUpload: FunctionComponent<ImageUploadProps> = () => {
       <Fab className='ImageUpload' color='primary' onClick={ handleOpen }>
         <Add/>
       </Fab>
-      <input accept='image/*' multiple={ true } ref={ ref => fileInput = ref }
-             onChange={(event) => handleFiles(event.target.files)} style={ {display: 'none'} } type='file'/>
+
+      <input
+        accept='image/*'
+        multiple={ true }
+        ref={ ref => fileInput = ref }
+        onChange={ handleFiles } style={ {display: 'none'} }
+        type='file'/>
     </>
   );
 };
